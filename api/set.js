77e2a05f7bf15af6cf59d4251cb1aea6455f5751
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-05-24 00:09:05 -0300

    Update set.js

--- api/set.js
+++ api/set.js
@@ -1,17 +1,15 @@
-let cache = ""
+let cache = "";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      const body = await req.json()
-      cache = body.codigo || ""
-      res.status(200).json({ status: "ok" })
+      const body = await req.json();
+      cache = body.codigo || "";
+      res.status(200).json({ status: "ok" });
     } catch (err) {
-      res.status(500).json({ error: "Erro no JSON" })
+      res.status(500).json({ error: "Erro no JSON" });
     }
   } else {
-    res.status(405).send("Método não permitido")
+    res.status(405).send("Método não permitido");
   }
 }
-
-export const config = { api: { bodyParser: false } }
